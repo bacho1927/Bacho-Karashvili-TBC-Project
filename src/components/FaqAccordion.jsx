cc064a16ad5a20634388ca2a9ed97aea25baf9c9
bacho1927 <bacho.karashvili@gmail.com>
2024-01-21 15:18:36 +0400

    .

--- src/components/FaqAccordion.jsx
+++ src/components/FaqAccordion.jsx
@@ -1,4 +1,4 @@
-import { FaqData } from "./faqData";
+import { FaqData } from "./FaqData";
 import Question from "./Question";
 function Faq() {
 
@@ -15,15 +15,12 @@ function Faq() {
                     <h1 className="text-[#F4F4F4] text-2xl ">ხშირად დასმული კითხვები</h1>
                     <h2 className="text-[#00A3E0] font-bold text-xl">ყველა კითხვა</h2>
                 </div>
-
                 <div className="pt-8 text-[#F4F4F4]">
                     <div className="   cursor-pointer  " >
 
                         {questions}
 
                     </div >
-
-
                 </div>
             </div>
         </div>
