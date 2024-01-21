6ad7bd50d6327426e37c95a696b7184345ce161a
bacho1927 <bacho.karashvili@gmail.com>
2024-01-21 16:25:58 +0400

    Update ParnetSlider.jsx

--- src/components/ParnetSlider.jsx
+++ src/components/ParnetSlider.jsx
@@ -52,7 +52,7 @@ function PartnerSlider() {
 
             <div className="max-w-[1000px]  m-auto  pt-8 ">
                 <h1 className="xsm:text-xl lg:text-2xl xsm:px-12 font-arial text-[#F4F4F4] ">პროექტის პარტნიორები</h1>
-                <div className="flex overflow-hidden xsm:gap-20 lg:gap-32 items-center py-28 justify-center" id="sliderContainer ">
+                <div className="xsm:flex-col flex overflow-hidden  xsm:gap-4 lg:gap-32 items-center py-28 justify-center" id="sliderContainer ">
 
                     {SliderData.slice(currentIndex, currentIndex + 3).map((image, index) => (
                         <img key={index} src={image} alt={`Image ${index + 1}`} className="xsm:w-36 xl:w-60" />
