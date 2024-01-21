ca500501b2e27438b2b6af12346a25aee5ac3e2c
bacho1927 <bacho.karashvili@gmail.com>
2024-01-21 17:06:54 +0400

    .

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@
 
 function Sidebar({ closeSidebar }) {
     return (
-        <div className="absolute h-[1000px] h-screen  inset-0 bg-gray-800 bg-opacity-75" onClick={closeSidebar}>
+        <div className="absolute h-screen  inset-0 bg-gray-800 bg-opacity-75" onClick={closeSidebar}>
 
             <div className="bg-white p-4 h-full">
                 <ul>
