aa77ca5bb27dfff531abdbad3f0be87961752203
bacho1927 <bacho.karashvili@gmail.com>
2024-01-22 21:07:49 +0400

    Update NavSidebar.jsx

--- src/components/NavSidebar.jsx
+++ src/components/NavSidebar.jsx
@@ -10,7 +10,7 @@ function NavSidebar({ closeSidebar, showSidebar }) {
             <div className="bg-[#1C1C1C]  h-full relative ">
                 <RxCross2 className="absolute text-white top-12 right-12 text-5xl" onClick={closeSidebar} />
 
-                <ul className="text-white right-6 top-16 text-right mt-14 p-4 space-y-6 absolute text-2xl">
+                <ul className="text-white right-6 top-28 text-right mt-14 p-4 space-y-10 absolute text-3xl">
                     <li><a href="/">მთავარი</a></li>
                     <li><a href="/">TBC IT</a></li>
                     <li><a href="/">TBC x USAID</a></li>
