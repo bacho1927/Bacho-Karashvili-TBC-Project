836defc400f612ec3a554eb59d5830f29fbe935f
bacho1927 <bacho.karashvili@gmail.com>
2024-01-22 21:05:40 +0400

    Update NavSidebar.jsx

--- src/components/NavSidebar.jsx
+++ src/components/NavSidebar.jsx
@@ -1,3 +1,6 @@
+import { RxCross2 } from "react-icons/rx";
+
+
 function NavSidebar({ closeSidebar, showSidebar }) {
 
 
@@ -5,8 +8,9 @@ function NavSidebar({ closeSidebar, showSidebar }) {
         <div className=" fixed  w-[400px]  inset-0 right-0 left-auto bg-opacity-75" >
 
             <div className="bg-[#1C1C1C]  h-full relative ">
-                <h1 className="absolute right-4 p-8 text-gray-200 text-4xl" onClick={closeSidebar}>X</h1>
-                <ul className="text-white right-8 top-16 text-right mt-14 p-4 space-y-6 absolute">
+                <RxCross2 className="absolute text-white top-12 right-12 text-5xl" onClick={closeSidebar} />
+
+                <ul className="text-white right-6 top-16 text-right mt-14 p-4 space-y-6 absolute text-2xl">
                     <li><a href="/">მთავარი</a></li>
                     <li><a href="/">TBC IT</a></li>
                     <li><a href="/">TBC x USAID</a></li>
