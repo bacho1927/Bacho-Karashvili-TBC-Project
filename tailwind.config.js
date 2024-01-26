381b1d521b8912d82545c2926096ded70404671b
bacho1927 <bacho.karashvili@gmail.com>
2024-01-26 21:37:12 +0400

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,27 +28,7 @@ export default {
         "0%": { opacity: 0 },
         "100%": { opacity: 1 },
       },
-      sideReveal: {
-        '0%': {
-
-          transform: 'translateX(100%)',
-        },
-        '100%': {
-
-          transform: 'translateX(0)',
-        },
-
-      },
-      closeSidebar: {
-        '0%': {
-
-          transform: 'translateX(0)',
-        },
-        '100%': {
-
-          transform: 'translateX(100%)',
-        },
-      },
+     
     },
     plugins: [],
   }
